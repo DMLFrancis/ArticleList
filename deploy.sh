3cca1bafb5cb5e0d1a27460fdc648d87ed85f410
Your Name <my@email.org>
2024-05-08 21:44:16 +0100

    updated

--- deploy.sh
+++ deploy.sh
@@ -12,18 +12,10 @@ fi
 sudo docker pull $IMAGE_NAME
 
 # Check if a docker container exists with the name of $CONTAINER_NAME if it does reomve the
-CONTAINER_EXISTS=$(sudo docker ps -a | grep article_list_app)
+CONTAINER_EXISTS=$(sudo docker ps -a | grep $CONTAINER_NAME)
 if [ "$CONTAINER_EXISTS" ]
 then
-  sudo docker rm "article_list_app"
+  sudo docker rm $CONTAINER_NAME
 fi
 
-sudo docker create -p 3000:3000 -p 8443:8443 --name article_list_app "$IMAGE_NAME"
-
-echo "$PRIVATE_KEY" > privatekey.pem
-echo "$SERVER" > server.crt
-
-docker cp ./privatekey.pem "article_list_app:/privatekey.pem"
-docker cp ./server.crt "article_list_app:/server.crt"
-
-docker start "article_list_app"
+sudo docker run -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME
